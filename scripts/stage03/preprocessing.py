b496028ea96900d0f6e5556f9a8c8a9fd4214436
PodYapolskiy <podyapolskiyaa@gmail.com>
2025-05-09 20:09:20 +0300

    feat: divide main on multiple spark jobs

---
+++ scripts/stage03/preprocessing.py
@@ -0,0 +1,202 @@
+import time
+import math
+
+from pyspark.sql import SparkSession
+from pyspark.sql import functions as F
+from pyspark.sql.dataframe import DataFrame
+from pyspark.ml import Pipeline
+
+# from pyspark.ml.regression import LinearRegression, RandomForestRegressor, GBTRegressor
+# from pyspark.ml.tuning import ParamGridBuilder, CrossValidator
+# from pyspark.ml.evaluation import RegressionEvaluator
+from pyspark.ml.base import Transformer
+from pyspark.ml.param.shared import HasOutputCols
+from pyspark.ml.util import DefaultParamsReadable, DefaultParamsWritable
+from pyspark.ml.feature import (
+    OneHotEncoder,
+    VectorAssembler,
+    StandardScaler,
+)
+
+start_time = time.time()
+
+spark: SparkSession = (
+    SparkSession.builder.config(
+        "hive.metastore.uris", "thrift://hadoop-02.uni.innopolis.ru:9883"
+    )
+    .config("spark.sql.catalogImplementation", "hive")
+    .config("spark.sql.warehouse.dir", "project/hive/warehouse")
+    .config("spark.sql.catalogImplementation", "hive")
+    .config("spark.sql.avro.compression.codec", "snappy")
+    .config("spark.log.level", "DEBUG")
+    .config("spark.ui.port", "4050")
+    .enableHiveSupport()
+    .getOrCreate()
+)
+
+df = spark.table("team18_projectdb.green_tripdata_monthly")
+df = df.sample(fraction=0.1, seed=42)
+
+# todo: drop rows
+
+# Handle null for encoding
+df = df.fillna(
+    value=99,  # stated in dataset description
+    subset=["vendorid", "ratecodeid", "payment_type", "trip_type"],
+)
+
+#########
+# SPLIT #
+#########
+train_df, test_df = df.randomSplit([0.8, 0.2], seed=42)
+
+#################
+# PREPROCESSING #
+#################
+# categorical
+encoder = OneHotEncoder(
+    inputCols=["vendorid", "ratecodeid", "payment_type", "trip_type"],
+    outputCols=[
+        "vendor_encoded",
+        "ratecode_encoded",
+        "payment_encoded",
+        "trip_encoded",
+    ],
+    handleInvalid="keep",
+)
+categorical_assembler = VectorAssembler(
+    inputCols=[
+        "vendor_encoded",
+        "ratecode_encoded",
+        "payment_encoded",
+        "trip_encoded",
+    ],
+    outputCol="categorical_features",
+    handleInvalid="skip",
+)
+
+
+# datetime
+class DateTimeTransformer(
+    Transformer, HasOutputCols, DefaultParamsReadable, DefaultParamsWritable
+):
+    def _transform(self, df: DataFrame) -> DataFrame:
+        df2: DataFrame = (
+            df.withColumn("year", F.year("lpep_pickup_datetime"))
+            # month [1, 12]
+            .withColumn("month", F.month("lpep_pickup_datetime"))
+            .withColumn("month_sin", F.sin(2 * math.pi * F.col("month") / 12))
+            .withColumn("month_cos", F.cos(2 * math.pi * F.col("month") / 12))
+            # .withColumn("month_day", F.dayofmonth("lpep_pickup_datetime"))
+            # week day [1, 7]
+            .withColumn("week_day", F.dayofweek("lpep_pickup_datetime"))
+            .withColumn("week_day_sin", F.sin(2 * math.pi * F.col("week_day") / 7))
+            .withColumn("week_day_cos", F.cos(2 * math.pi * F.col("week_day") / 7))
+            # hour [0, 23]
+            .withColumn("hour", F.hour("lpep_pickup_datetime"))
+            .withColumn("hour_sin", F.sin(2 * math.pi * F.col("hour") / 24))
+            .withColumn("hour_cos", F.cos(2 * math.pi * F.col("hour") / 24))
+            # minute [0, 59]
+            .withColumn("minute", F.minute("lpep_pickup_datetime"))
+            .withColumn("minute_sin", F.sin(2 * math.pi * F.col("minute") / 60))
+            .withColumn("minute_cos", F.cos(2 * math.pi * F.col("minute") / 60))
+            # second [0, 59]
+            .withColumn("second", F.second("lpep_pickup_datetime"))
+            .withColumn("second_sin", F.sin(2 * math.pi * F.col("second") / 60))
+            .withColumn("second_cos", F.cos(2 * math.pi * F.col("second") / 60))
+            # .withColumn(
+            #     "trip_time",
+            #     F.col("lpep_dropoff_datetime").cast("long")
+            #     - F.col("lpep_pickup_datetime").cast("long"),
+            # )
+        )
+        return df2.drop("month", "week_day", "hour", "minute", "second")
+
+
+date_time_trasformer = DateTimeTransformer()
+time_decomposer = VectorAssembler(
+    inputCols=[
+        "month_sin",
+        "month_cos",
+        "week_day_sin",
+        "week_day_cos",
+        "hour_sin",
+        "hour_cos",
+        "minute_sin",
+        "minute_cos",
+        "second_sin",
+        "second_cos",
+    ],
+    outputCol="datetime_features",
+)
+
+# numerical columns
+num_assembler = VectorAssembler(
+    inputCols=[
+        # "year",
+        "passenger_count",
+        "trip_distance",
+        # less informative
+        "extra",
+        "mta_tax",
+        "tip_amount",
+        "tolls_amount",
+    ],
+    outputCol="num_features",
+    handleInvalid="skip",
+)
+scaler = StandardScaler(inputCol="num_features", outputCol="scaled_num_features")
+
+# final and model
+final_assembler = VectorAssembler(
+    inputCols=[
+        # categorical
+        "categorical_features",
+        # datetime
+        "datetime_features",
+        # numerical
+        "scaled_num_features",
+    ],
+    outputCol="features",
+)
+
+pipeline = Pipeline(
+    stages=[
+        # categorical
+        encoder,  # one hot encode
+        categorical_assembler,
+        # time
+        date_time_trasformer,
+        time_decomposer,
+        # numerical
+        num_assembler,
+        scaler,
+        # final assembler
+        final_assembler,
+    ]
+)
+
+pipeline_model = pipeline.fit(train_df)
+preprocessed_train_df = pipeline_model.transform(train_df)
+preprocessed_test_df = pipeline_model.transform(test_df)
+
+(
+    preprocessed_train_df.select("features", "fare_amount")
+    .repartition(1)
+    .write.mode("overwrite")
+    .option("compression", "snappy")
+    .parquet("project/data/train")
+)
+(
+    preprocessed_test_df.select("features", "fare_amount")
+    .repartition(1)
+    .write.mode("overwrite")
+    .option("compression", "snappy")
+    .parquet("project/data/test")
+)
+
+end_time = time.time()
+elapsed_time = end_time - start_time
+print(f"Time elapsed: {elapsed_time:.2f} seconds")
+
+spark.stop()
--- scripts/stage3.sh
+++ scripts/stage3.sh
@@ -18,55 +18,140 @@ BIN="$SCRIPTS/bin"
 hdfs dfs -mkdir -p $HDFS_ROOT/project/output
 hdfs dfs -mkdir -p $HDFS_ROOT/project/models
 
-rm -rf $SCRIPTS/stage03/.venv
-python3 -m venv $SCRIPTS/stage03/.venv
-source $SCRIPTS/stage03/.venv/bin/activate
-pip install -r $SCRIPTS/stage03/requirements.txt
-rm -rf $BIN/.venv.tar.gz
-$SCRIPTS/stage03/.venv/bin/venv-pack -o $BIN/.venv.tar.gz
-
-export PYSPARK_DRIVER_PYTHON=".venv/bin/python"
-export PYSPARK_PYTHON="$SCRIPTS/stage03/.venv/bin/python"
-
+#################
+# Preprocessing #
+#################
+echo "Submitting preprocessing job..."
 spark-submit \
+    --name "Team18 | Spark ML | Preprocessing" \
     --master yarn \
-    --archives $BIN/.venv.tar.gz#.venv \
-    --deploy-mode cluster \
     --driver-memory 2g \
-    --executor-memory 8g \
-    --executor-cores 8 \
-    $SCRIPTS/stage03/main.py
+    --num-executors 3 \
+    --executor-memory 4g \
+    --executor-cores 5 \
+    $SCRIPTS/stage03/preprocessing.py
 
-# --packages org.apache.spark:spark-avro_2.12:3.5.1 \
-# --packages ai.catboost:catboost-spark_3.5_2.12 \
-# --packages org.apache.hadoop:hadoop-aws:3.4.1 \
-
-if [ $? -eq 1 ]; then
-    echo "Stage 3 failed."
-    exit 1
+# Check if preprocessing succeeded
+if [ $? -ne 0 ]; then
+  echo "Preprocessing failed. Aborting..."
+  exit 1
 fi
 
-########
-# Data #
-########
-hdfs dfs -copyToLocal $HDFS_ROOT/project/data/train output/train.json
-hdfs dfs -copyToLocal $HDFS_ROOT/project/data/test output/test.json
+hdfs dfs -copyToLocal -f $HDFS_ROOT/project/data/train output
+hdfs dfs -copyToLocal -f $HDFS_ROOT/project/data/test output
+
 
 #####################
 # Linear Regression #
 #####################
-hdfs dfs -copyToLocal $HDFS_ROOT/project/models/model1 models/model1
-hdfs dfs -copyToLocal $HDFS_ROOT/project/output/model1_predictions output/model1_predictions.csv
+echo "Submitting Linear Regression job..."
+spark-submit \
+    --name "Team18 | Spark ML | Linear Regression" \
+    --master yarn \
+    --driver-memory 2g \
+    --num-executors 3 \
+    --executor-memory 4g \
+    --executor-cores 5 \
+    $SCRIPTS/stage03/linear_regression.py
+
+if [ $? -ne 0 ]; then
+  echo "Linear Regression failed. Aborting..."
+  exit 1
+fi
+
+hdfs dfs -copyToLocal -f $HDFS_ROOT/project/models/model1 models/
+hdfs dfs -copyToLocal -f $HDFS_ROOT/project/output/model1_predictions output/
+
+exit 0
+
 
 ###########################
 # Random Forest Regressor #
 ###########################
-hdfs dfs -copyToLocal $HDFS_ROOT/project/models/model2 models/model2
-hdfs dfs -copyToLocal $HDFS_ROOT/project/output/model2_predictions output/model2_predictions.csv
+# echo "Submitting Random Forest Regressor job..."
+# spark-submit \
+#     --name "Team18 | Spark ML | Random Forest Regressor" \
+#     --master yarn \
+#     --driver-memory 2g \
+#     --num-executors 3 \
+#     --executor-memory 4g \
+#     --executor-cores 5 \ q
+#     $SCRIPTS/stage03/random_forest.py
+
+# if [ $? -ne 0 ]; then
+#   echo "Random Forest failed. Aborting..."
+#   exit 1
+# fi
+
+# hdfs dfs -copyToLocal -f $HDFS_ROOT/project/models/model2 models/
+# hdfs dfs -copyToLocal -f $HDFS_ROOT/project/output/model2_predictions output/
+
+# exit 0
+
+###########################
+# Catboost Regressor #
+###########################
+# echo "Submitting Boosting Regressor job..."
+# spark-submit \
+#     --master yarn \
+#     --driver-memory 2g \
+#     --num-executors 3 \
+#     --executor-memory 4g \
+#     --executor-cores 5 \
+#     --packages ai.catboost:catboost-spark_3.5_2.12:1.2.8 \
+#     $SCRIPTS/stage03/catboost.py
+
+# if [ $? -ne 0 ]; then
+#   echo "Catboost failed. Aborting..."
+#   exit 1
+# fi
+
+# exit 0
+
 
 ##############
 # Comparison #
 ##############
-hdfs dfs -copyToLocal $HDFS_ROOT/project/output/evaluation output/evaluation.csv
+
+hdfs dfs -copyToLocal -f $HDFS_ROOT/project/output/evaluation output/
 
 echo "Stage 3 completed successfully."
+
+
+
+# rm -rf $SCRIPTS/stage03/.venv
+# python3 -m venv $SCRIPTS/stage03/.venv
+# source $SCRIPTS/stage03/.venv/bin/activate
+# pip install -r $SCRIPTS/stage03/requirements.txt
+# rm -rf $BIN/.venv.tar.gz
+# $SCRIPTS/stage03/.venv/bin/venv-pack -o $BIN/.venv.tar.gz
+
+# export PYSPARK_DRIVER_PYTHON=".venv/bin/python"
+# export PYSPARK_PYTHON="$SCRIPTS/stage03/.venv/bin/python"
+
+# --archives $BIN/.venv.tar.gz#.venv \
+# --deploy-mode cluster \
+
+
+# spark-submit \
+#     --master yarn \
+#     --driver-memory 2g \
+#     --num-executors 3 \
+#     --executor-memory 4g \
+#     --executor-cores 5 \
+#     $SCRIPTS/stage03/main.py
+
+# --packages org.apache.spark:spark-avro_2.12:3.5.1 \
+# --packages org.apache.hadoop:hadoop-aws:3.4.1 \
+
+# if [ $? -eq 1 ]; then
+#     echo "Stage 3 failed."
+#     exit 1
+# fi
+
+###############################
+# Gradient Boosting Regressor #
+###############################
+# hdfs dfs -copyToLocal $HDFS_ROOT/project/models/model1 models/model1
+# hdfs dfs -cat project/output/model3_predictions.csv/*.csv > output/model3_predictions.csv
+
